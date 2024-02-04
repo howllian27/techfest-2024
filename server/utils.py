a7749e0f1597a1ce03a500573cd0c8b969717196
XeuniceX <Eunice.ernqi@gmail.com>
2024-02-04 18:26:09 +0800

    prompt updates

--- server/utils.py
+++ server/utils.py
@@ -6,7 +6,7 @@ def song_reccommender (input):
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are an expert content creator who is skilled in picking trendy musics suitable for different content."},
+            {"role": "system", "content": "You are an expert content creator who is skilled in picking trendy musics (No need for any elaboration) suitable for different content and designed to output in json. Provide me with the title, artist, youtube link and spotify link of the song"},
             {"role": "user", "content": f"{input}"}
         ]
     )
