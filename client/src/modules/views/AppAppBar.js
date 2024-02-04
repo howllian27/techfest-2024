c5b7fd0d518d2e113a651d66077566a0b7c46be4
chenglin2003 <133096344+chenglin2003@users.noreply.github.com>
2024-02-04 21:17:25 +0800

    commit second page

--- client/src/modules/views/AppAppBar.js
+++ client/src/modules/views/AppAppBar.js
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import AppBar from '../components/AppBar';
 import Toolbar from '../components/Toolbar';
+import { Link as RouterLink } from "react-router-dom";
 
 const rightLink = {
   fontSize: 16,
@@ -16,11 +17,12 @@ function AppAppBar() {
       <AppBar position="fixed">
         <Toolbar sx={{ justifyContent: 'space-between' }}>
           <Box sx={{ flex: 1 }} />
-          <Link
+          <Link 
+            component={RouterLink}
+            to="/"
             variant="h6"
             underline="none"
             color="inherit"
-            href="/premium-themes/onepirate/"
             sx={{ fontSize: 24 }}
           >
             {'content.AI'}
--- client/src/pages/ChoicesPage.js
+++ client/src/pages/ChoicesPage.js
@@ -1,12 +1,16 @@
 // src/SelectChoices.js
 import React, { useState } from 'react';
 import AppAppBar from '../modules/views/AppAppBar';
+import Choices from '../modules/components/Choices';
 
 function ChoicesPage(){
     return (
         <div>
-          <AppAppBar>
-          </AppAppBar>
+          <AppAppBar/>
+          <Choices/>
+          <Choices/>
+          <Choices/>
+
         </div>
       );
     
